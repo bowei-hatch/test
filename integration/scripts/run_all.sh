97899e634481c9ff2c4d8ef7b590671054120631
Bowei Liu <liubowei@gmail.com>
2021-11-16 15:44:03 -0800

    update

--- integration/scripts/run_all.sh
+++ integration/scripts/run_all.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo 'this is run_all.sh'
+set -xeo
 pwd
-"$pwd"/should_pass.sh || exit 1
-#"$pwd"/should_fail.sh || exit 1
+"$pwd"/should_pass.sh
+#"$pwd"/should_fail.sh
 echo 'done running all!'
