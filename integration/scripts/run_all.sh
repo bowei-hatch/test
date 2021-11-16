3d95563fbfb74ddc26600ebc0224db517f9a7390
Bowei Liu <liubowei@gmail.com>
2021-11-16 15:49:28 -0800

    update

--- integration/scripts/run_all.sh
+++ integration/scripts/run_all.sh
@@ -4,5 +4,5 @@ echo 'this is run_all.sh'
 set -xeo pipefail
 pwd
 bash integration/scripts/should_pass.sh
-#bash integration/scripts/should_fail.sh
+bash integration/scripts/should_fail.sh
 echo 'done running all!'
